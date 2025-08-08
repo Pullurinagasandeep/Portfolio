ac097822666b144eec9d10fc3a4e90246d154d79
Sandeep <160137919+Pullurinagasandeep@users.noreply.github.com>
2025-08-08 21:38:41 +0530

    successfully updated hammenu

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ let hamopen = document.querySelector("#ham-open");
 let hamclose = document.querySelector("#ham-close");
 let main = document.querySelector("main");
 
+let removeHamLinks = () =>{
+    hamLinks.classList.remove("show");
+    hamopen.classList.remove("display");
+    hamclose.classList.remove("display");
+};
+
 hamicons.addEventListener("click",() =>{
     hamLinks.classList.toggle("show");
     hamopen.classList.toggle("display");
     hamclose.classList.toggle("display");
 });
 
-hamLinks.addEventListener("click",() =>{
-    
-    hamLinks.classList.remove("show");
-    hamopen.classList.toggle("display");
-    hamclose.classList.toggle("display");
-});
+hamLinks.addEventListener("click",removeHamLinks);
+main.addEventListener("click",removeHamLinks);
+window.addEventListener("scroll",removeHamLinks);
+
+
 
 
 
